768e7f2bb96974bc1cc995645f13f94dcdcfd5e4
Mostapha-El-Kaddaoui <73528791+Mostapha-El-Kaddaoui@users.noreply.github.com>
2025-03-19 10:09:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,8 @@ app = Flask(__name__)
 @app.route('/phonecall', methods=['POST'])
 def PhoneCall():
     try:
-        # Load the image
-        url = "https://github.com/Mostapha-El-Kaddaoui/Server-Flask-Railway/blob/main/compressed_image.jpg"
-        image = Image.open(BytesIO(requests.get(url).content))
+        url = "https://raw.githubusercontent.com/Mostapha-El-Kaddaoui/Server-Flask-Railway/refs/heads/main/image2.jpg"
+        image = Image.open(requests.get(url, stream=True).raw)  # Ensure to use BytesIO for image content
 
         # Initialize the processor and model
         processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
